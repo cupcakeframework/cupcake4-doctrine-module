0a57cfe511eb60cf1b9f2c5ebe476afb7405f32e
Ricardo Fiorani <ricardo.fiorani@gmail.com>
2015-09-10 14:19:47 -0300

    first commit

---
+++ src/Factory/EntityManagerFactory.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace CupcakeDoctrineModule\Factory;
+
+use Cupcake\Config\ConfigManager;
+use Cupcake\Service\ServiceManager;
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+/**
+ * @author Ricardo Fiorani
+ */
+class EntityManagerFactory
+{
+
+    /**
+     * @param ServiceManager $serviceManager
+     * @return EntityManager
+     */
+    public function __invoke(ServiceManager $serviceManager)
+    {
+        /* @var $configManager ConfigManager */
+        $configManager = $serviceManager->get('ConfigManager');
+        $databaseConfig = $configManager->get('database');
+        $doctrineConfig = $configManager->get('doctrine');
+
+        $connectionParams = array(
+            'dbname' => $databaseConfig['dbname'],
+            'user' => $databaseConfig['user'],
+            'password' => $databaseConfig['password'],
+            'host' => $databaseConfig['host'],
+            'driver' => 'pdo_mysql',
+            'charset' => 'utf8',
+            'driverOptions' => array(
+                1002 => 'SET NAMES utf8'
+            ),
+        );
+
+        $entitiesPaths = $doctrineConfig['entitiesPaths'];
+        $proxyDir = $doctrineConfig['proxyDir'];
+        $isDevMode = $configManager->get('debug');
+
+        $setupConfig = Setup::createAnnotationMetadataConfiguration($entitiesPaths, $isDevMode, $proxyDir);
+        $setupConfig->addCustomStringFunction('rand', 'Mapado\MysqlDoctrineFunctions\DQL\MysqlRand');
+        $entityManager = EntityManager::create($connectionParams, $setupConfig);
+        $platform = $entityManager->getConnection()->getDatabasePlatform();
+        $platform->registerDoctrineTypeMapping('enum', 'string');
+
+        return $entityManager;
+    }
+
+}
